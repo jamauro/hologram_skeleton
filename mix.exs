893e923b8c88e5c03eb2fd10ff4fb8396421dd0b
Bart Blast <bart@bartblast.com>
2026-05-31 12:57:39 +0200

    Update Hologram dep

--- mix.exs
+++ mix.exs
@@ -59,7 +59,7 @@ defmodule HologramSkeleton.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:hologram, "~> 0.8.3"}
+      {:hologram, "~> 0.9.1"}
     ]
   end
 
